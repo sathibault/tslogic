3e088efe1aa80ef5e31e902ac4c42f6285827de6
Sheetal Nandi <shkamat@microsoft.com>
2022-06-30 15:43:32 -0700

    Add new option to cacheResolution (No actual functionality yet)

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -6773,6 +6773,7 @@ export interface CompilerOptions {
     jsxImportSource?: string;
     composite?: boolean;
     incremental?: boolean;
+    cacheResolutions?: boolean;
     tsBuildInfoFile?: string;
     removeComments?: boolean;
     rootDir?: string;
--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -7082,6 +7082,7 @@ declare namespace ts {
         jsxImportSource?: string;
         composite?: boolean;
         incremental?: boolean;
+        cacheResolutions?: boolean;
         tsBuildInfoFile?: string;
         removeComments?: boolean;
         rootDir?: string;
