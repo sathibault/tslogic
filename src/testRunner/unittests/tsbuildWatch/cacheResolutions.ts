mangalarora/TypeScript

Add tests where module resolution caches should reuse the resolutions from buildInfo

src/testRunner/unittests/tsbuildWatch/cacheResolutions.ts
@@ -135,9 +135,21 @@ namespace ts.tscWatch.cacheResolutions {
         });
 
         describe("resolution reuse from multiple places", () => {
-            verifyTscWatchMultiFile("caching resolutions when resolution reused from multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
-            verifyTscWatchMultiFile("caching resolutions when resolution reused from multiple places when already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
-            function verifyTscWatchMultiFile(subScenario: string, sys: () => WatchedSystem) {
+            verifyTscWatchMultiPlaces("caching resolutions when resolution reused from multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
+            verifyTscWatchMultiPlaces("caching resolutions when resolution reused from multiple places when already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
+            verifyTscWatch({
+                scenario: "cacheResolutions",
+                subScenario: "caching resolutions when resolution reused from multiple places first pass",
+                sys: () => {
+                    const sys = getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild();
+                    sys.prependFile("/src/project/d/da/daa/daaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`);
+                    return sys;
+                },
+                commandLineArgs: ["-b", "-w", "--explainFiles"],
+                changes: emptyArray
+            });
+
+            function verifyTscWatchMultiPlaces(subScenario: string, sys: () => WatchedSystem) {
                 verifyTscWatch({
                     scenario: "cacheResolutions",
                     subScenario,
@@ -157,6 +169,16 @@ namespace ts.tscWatch.cacheResolutions {
                         {
                             caption: "modify c/ca/caa/randomFileForImport by adding import",
                             change: sys => sys.prependFile("/src/project/c/ca/caa/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                            timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                        },
+                        {
+                            caption: "modify d/da/daa/daaa/x/y/z/randomFileForImport by adding import",
+                            change: sys => sys.prependFile("/src/project/d/da/daa/daaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                            timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                        },
+                        {
+                            caption: "modify e/ea/eaa/eaaa/x/y/z/randomFileForImport by adding import",
+                            change: sys => sys.prependFile("/src/project/e/ea/eaa/eaaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
                             timeouts: sys => sys.runQueuedTimeoutCallbacks(),
                         },
                     ]

src/testRunner/unittests/tscWatch/cacheResolutions.ts
@@ -149,9 +149,20 @@ namespace ts.tscWatch.cacheResolutions {
         });
 
         describe("resolution reuse from multiple places", () => {
-            verifyTscWatchMultiFile("caching resolutions when resolution reused from multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
-            verifyTscWatchMultiFile("caching resolutions when resolution reused from multiple places when already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
-            function verifyTscWatchMultiFile(subScenario: string, sys: () => WatchedSystem) {
+            verifyTscWatchMultiPlaces("caching resolutions when resolution reused from multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
+            verifyTscWatchMultiPlaces("caching resolutions when resolution reused from multiple places when already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
+            verifyTscWatch({
+                scenario: "cacheResolutions",
+                subScenario: "caching resolutions when resolution reused from multiple places first pass",
+                sys: () => {
+                    const sys = getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild();
+                    sys.prependFile("/src/project/d/da/daa/daaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`);
+                    return sys;
+                },
+                commandLineArgs: ["-w", "--explainFiles"],
+                changes: emptyArray
+            });
+            function verifyTscWatchMultiPlaces(subScenario: string, sys: () => WatchedSystem) {
                 verifyTscWatch({
                     scenario: "cacheResolutions",
                     subScenario,
@@ -171,6 +182,16 @@ namespace ts.tscWatch.cacheResolutions {
                         {
                             caption: "modify c/ca/caa/randomFileForImport by adding import",
                             change: sys => sys.prependFile("/src/project/c/ca/caa/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                            timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                        },
+                        {
+                            caption: "modify d/da/daa/daaa/x/y/z/randomFileForImport by adding import",
+                            change: sys => sys.prependFile("/src/project/d/da/daa/daaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                            timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                        },
+                        {
+                            caption: "modify e/ea/eaa/eaaa/x/y/z/randomFileForImport by adding import",
+                            change: sys => sys.prependFile("/src/project/e/ea/eaa/eaaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
                             timeouts: sys => sys.runQueuedTimeoutCallbacks(),
                         },
                     ]

sathibault/tslogic

src/testRunner/unittests/tsbuildWatch/cacheResolutions.ts
@@ -1,3 +1,4 @@
+import * as ts from "../../_namespaces/ts";
 import {
     TestServerHost,
 } from "../virtualFileSystemWithWatch";
@@ -153,9 +154,20 @@ describe("unittests:: tsbuildWatch:: watchMode:: cacheResolutions::", () => {
     });
 
     describe("resolution reuse from multiple places", () => {
-        verifyTscWatchMultiFile("multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
-        verifyTscWatchMultiFile("multiple places already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
-        function verifyTscWatchMultiFile(subScenario: string, sys: () => TestServerHost) {
+        verifyTscWatchMultiPlaces("multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
+        verifyTscWatchMultiPlaces("multiple places already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
+        verifyTscWatch({
+            scenario: "cacheResolutions",
+            subScenario: "multiple places first pass",
+            sys: () => {
+                const sys = getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild();
+                sys.prependFile("/src/project/d/da/daa/daaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`);
+                return sys;
+            },
+            commandLineArgs: ["-b", "-w", "--explainFiles"],
+            changes: ts.emptyArray
+        });
+        function verifyTscWatchMultiPlaces(subScenario: string, sys: () => TestServerHost) {
             verifyTscWatch({
                 scenario: "cacheResolutions",
                 subScenario,
@@ -175,6 +187,16 @@ describe("unittests:: tsbuildWatch:: watchMode:: cacheResolutions::", () => {
                     {
                         caption: "modify c/ca/caa/randomFileForImport by adding import",
                         change: sys => sys.prependFile("/src/project/c/ca/caa/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                        timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                    },
+                    {
+                        caption: "modify d/da/daa/daaa/x/y/z/randomFileForImport by adding import",
+                        change: sys => sys.prependFile("/src/project/d/da/daa/daaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                        timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                    },
+                    {
+                        caption: "modify e/ea/eaa/eaaa/x/y/z/randomFileForImport by adding import",
+                        change: sys => sys.prependFile("/src/project/e/ea/eaa/eaaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
                         timeouts: sys => sys.runQueuedTimeoutCallbacks(),
                     },
                 ]

src/testRunner/unittests/tscWatch/cacheResolutions.ts
@@ -1,3 +1,4 @@
+import * as ts from "../../_namespaces/ts";
 import {
     getPkgImportContent,
     getPkgTypeRefContent,
@@ -163,9 +164,20 @@ describe("unittests:: tsc-watch:: cacheResolutions::", () => {
     });
 
     describe("resolution reuse from multiple places", () => {
-        verifyTscWatchMultiFile("multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
-        verifyTscWatchMultiFile("multiple places already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
-        function verifyTscWatchMultiFile(subScenario: string, sys: () => TestServerHost) {
+        verifyTscWatchMultiPlaces("multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
+        verifyTscWatchMultiPlaces("multiple places already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
+        verifyTscWatch({
+            scenario: "cacheResolutions",
+            subScenario: "multiple places first pass",
+            sys: () => {
+                const sys = getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild();
+                sys.prependFile("/src/project/d/da/daa/daaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`);
+                return sys;
+            },
+            commandLineArgs: ["-w", "--explainFiles"],
+            changes: ts.emptyArray
+        });
+        function verifyTscWatchMultiPlaces(subScenario: string, sys: () => TestServerHost) {
             verifyTscWatch({
                 scenario: "cacheResolutions",
                 subScenario,
@@ -185,6 +197,16 @@ describe("unittests:: tsc-watch:: cacheResolutions::", () => {
                     {
                         caption: "modify c/ca/caa/randomFileForImport by adding import",
                         change: sys => sys.prependFile("/src/project/c/ca/caa/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                        timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                    },
+                    {
+                        caption: "modify d/da/daa/daaa/x/y/z/randomFileForImport by adding import",
+                        change: sys => sys.prependFile("/src/project/d/da/daa/daaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                        timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                    },
+                    {
+                        caption: "modify e/ea/eaa/eaaa/x/y/z/randomFileForImport by adding import",
+                        change: sys => sys.prependFile("/src/project/e/ea/eaa/eaaa/x/y/z/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
                         timeouts: sys => sys.runQueuedTimeoutCallbacks(),
                     },
                 ]