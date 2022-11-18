mangalarora/TypeScript

Test for module resolutions from different directories

src/testRunner/unittests/tscWatch/cacheResolutions.ts
@@ -147,5 +147,36 @@ namespace ts.tscWatch.cacheResolutions {
                 });
             }
         });
+
+        describe("resolution reuse from multiple places", () => {
+            verifyTscWatchMultiFile("caching resolutions when resolution reused from multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
+            verifyTscWatchMultiFile("caching resolutions when resolution reused from multiple places when already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
+            function verifyTscWatchMultiFile(subScenario: string, sys: () => WatchedSystem) {
+                verifyTscWatch({
+                    scenario: "cacheResolutions",
+                    subScenario,
+                    sys,
+                    commandLineArgs: ["-w", "--explainFiles"],
+                    changes: [
+                        {
+                            caption: "modify randomFileForImport by adding import",
+                            change: sys => sys.prependFile("/src/project/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                            timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                        },
+                        {
+                            caption: "modify b/randomFileForImport by adding import",
+                            change: sys => sys.prependFile("/src/project/b/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                            timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                        },
+                        {
+                            caption: "modify c/ca/caa/randomFileForImport by adding import",
+                            change: sys => sys.prependFile("/src/project/c/ca/caa/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                            timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                        },
+                    ]
+                });
+            }
+        });
+
     });
 }

sathibault/tslogic

src/testRunner/unittests/tscWatch/cacheResolutions.ts
@@ -6,6 +6,8 @@ import {
     getWatchSystemWithNode16WithBuild,
     getWatchSystemWithOut,
     getWatchSystemWithOutWithBuild,
+    getWatchSystemWithSameResolutionFromMultiplePlaces,
+    getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild,
 } from "../tsbuild/cacheResolutionsHelper";
 import {
     TestServerHost,
@@ -153,6 +155,36 @@ describe("unittests:: tsc-watch:: cacheResolutions::", () => {
                     {
                         caption: `modify ${file} by adding import`,
                         change: sys => sys.prependFile(`/src/project/${file}.ts`, `export type { ImportInterface0 } from "pkg0";\n`),
+                        timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                    },
+                ]
+            });
+        }
+    });
+
+    describe("resolution reuse from multiple places", () => {
+        verifyTscWatchMultiFile("multiple places", getWatchSystemWithSameResolutionFromMultiplePlaces);
+        verifyTscWatchMultiFile("multiple places already built", getWatchSystemWithSameResolutionFromMultiplePlacesWithBuild);
+        function verifyTscWatchMultiFile(subScenario: string, sys: () => TestServerHost) {
+            verifyTscWatch({
+                scenario: "cacheResolutions",
+                subScenario,
+                sys,
+                commandLineArgs: ["-w", "--explainFiles"],
+                changes: [
+                    {
+                        caption: "modify randomFileForImport by adding import",
+                        change: sys => sys.prependFile("/src/project/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                        timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                    },
+                    {
+                        caption: "modify b/randomFileForImport by adding import",
+                        change: sys => sys.prependFile("/src/project/b/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
+                        timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+                    },
+                    {
+                        caption: "modify c/ca/caa/randomFileForImport by adding import",
+                        change: sys => sys.prependFile("/src/project/c/ca/caa/randomFileForImport.ts", `import type { ImportInterface0 } from "pkg0";\n`),
                         timeouts: sys => sys.runQueuedTimeoutCallbacks(),
                     },
                 ]