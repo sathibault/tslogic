yjm123456789yjm/TypeScript

Add reusing cache stub

src/compiler/types.ts
@@ -4266,6 +4266,7 @@ namespace ts {
     /* @internal */
     export const enum StructureIsReused {
         Not,
+        SafeModuleCache,
         SafeModules,
         Completely,
     }
@@ -6593,6 +6594,15 @@ namespace ts {
         oldProgram?: Program;
         configFileParsingDiagnostics?: readonly Diagnostic[];
     }
+
+    /*@internal*/
+    export interface OldBuildInfoProgram {
+        isBuildInfoProgram: true; // temp -- later change this to some method thats distinct
+        getCompilerOptions(): CompilerOptions;
+    }
+
+    /*@internal*/
+    export type CreateProgramOptionsWithOldBuildInfoProgram = Omit<CreateProgramOptions, "oldProgram"> & { oldProgram?: OldBuildInfoProgram | Program; };
 
     /* @internal */
     export interface CommandLineOptionBase {

sathibault/tslogic

src/compiler/types.ts
@@ -4569,6 +4569,7 @@ export interface ResolvedProjectReference {
 /** @internal */
 export const enum StructureIsReused {
     Not,
+    SafeModuleCache,
     SafeModules,
     Completely,
 }
@@ -6965,6 +6966,22 @@ export interface CreateProgramOptions {
     oldProgram?: Program;
     configFileParsingDiagnostics?: readonly Diagnostic[];
 }
+
+/** @internal */
+export interface OldBuildInfoProgram {
+    getCompilerOptions(): CompilerOptions;
+}
+
+/** @internal */
+export interface OldBuildInfoProgramHost {
+    fileExists(fileName: string): boolean;
+}
+
+/** @internal */
+export type OldBuildInfoProgramConstructor = (host: OldBuildInfoProgramHost) => OldBuildInfoProgram | undefined;
+
+/** @internal */
+export type CreateProgramOptionsWithOldBuildInfoProgramConstructor = Omit<CreateProgramOptions, "oldProgram"> & { oldProgram?: Program | OldBuildInfoProgramConstructor; };
 
 /** @internal */
 export interface CommandLineOptionBase {