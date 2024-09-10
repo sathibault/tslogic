7f3c8d663fbd20454c1dd2f7f1dccf971ce7fe71
Scott Thibault <sathibault@yahoo.com>
2024-09-10 09:19:13 -0400

    Publish 5.0.18

--- lib/lib.es2020.d.ts
+++ lib/lib.es2020.d.ts
@@ -29,7 +29,14 @@ and limitations under the License.
 /// <reference lib="es2020.intl" />
 
 
-interface Bits {}
+interface Bits {
+  _bits: true;
+}
+
+interface RtlBits {
+  _rtl: true;
+}
+
 interface UInt<W extends number> extends Bits {
   signed: false;
   data: W;
@@ -38,6 +45,7 @@ interface Int<W extends number> extends Bits {
   signed: true;
   data: W;
 }
+
 type bit = UInt<1>;
 type uint8 = UInt<8>;
 type uint16 = UInt<16>;
@@ -49,13 +57,13 @@ type int32 = Int<32>;
 type int64 = Int<64>;
 type float32 = number;
 type float64 = number;
-declare function uint8(x: Bits): UInt<8>;
-declare function uint16(x: Bits): UInt<16>;
-declare function uint32(x: Bits): UInt<32>;
-declare function uint64(x: Bits): UInt<64>;
-declare function int8(x: Bits): Int<8>;
-declare function int16(x: Bits): Int<16>;
-declare function int32(x: Bits): Int<32>;
-declare function int64(x: Bits): Int<64>;
-declare function float32(x: Bits): number;
-declare function float64(x: Bits): number;
+declare function uint8(x: number|Bits|RtlBits): UInt<8>;
+declare function uint16(x: number|Bits|RtlBits): UInt<16>;
+declare function uint32(x: number|Bits|RtlBits): UInt<32>;
+declare function uint64(x: number|Bits|RtlBits): UInt<64>;
+declare function int8(x: number|Bits|RtlBits): Int<8>;
+declare function int16(x: number|Bits|RtlBits): Int<16>;
+declare function int32(x: number|Bits|RtlBits): Int<32>;
+declare function int64(x: number|Bits|RtlBits): Int<64>;
+declare function float32(x: number|Bits|RtlBits): number;
+declare function float64(x: number|Bits|RtlBits): number;
